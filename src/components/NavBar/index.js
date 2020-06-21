053ded843a60c28d152d0c1e0f63c89319e78882
Shani Almog <killerdrum@gmail.com>
2020-06-21 21:57:58 +0300

    add active navbar with react-router-dom

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Styled from 'styled-components'
 
 // import Button from '../Button'
 
-    const NavButton = Styled.button`
+const StyledNav = Styled(NavLink)`
     font-size: 1rem;
-    padding: .5rem 1rem;
+    padding: .5rem 1.1rem;
+    /* margin: .5rem; */
     border: none;
     background-color: #fff;
+    text-decoration: none;
+    box-sizing: content-box;
+    color: #000;
     :hover {
-        background-color: #e9e9e9;
+        background-color: #f1f1f1;
     }
-    :active {
-        background-color: #e1e1e1;
+    /* :active {
+        background-color: #e1e1e1; */
     }
     border-radius: 3px;
     `
@@ -21,9 +25,22 @@ import Styled from 'styled-components'
 const NavBar = () => {
     return (
         <div>
-            <Link to="/"><NavButton>Home</NavButton ></Link>
-            <Link to="/about"><NavButton>About</NavButton ></Link>
-            <Link to="/contact"><NavButton>Contact</NavButton ></Link>
+            <StyledNav
+                to="/"
+                exact
+                // activeStyle={{ borderBottom: '3px solid #e1e1e1' }}>
+                activeStyle={{ backgroundColor: '#e1e1e1' }}>
+                Home</StyledNav>
+            <StyledNav
+                to="/about"
+                activeStyle={{ backgroundColor: '#e1e1e1' }}>
+                About
+                </StyledNav>
+            <StyledNav
+                to="/contact"
+                activeStyle={{ backgroundColor: '#e1e1e1' }}>
+                Contact
+                 </StyledNav>
         </div>
     )
 }
